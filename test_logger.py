bfe43757c50329e892afe4f42f4ee1d65095cfff
lokeshlodhi1 <choice.employee1130@gmail.com>
2026-06-28 22:13:28 +0530

    Create test_logger.py

---
+++ test_logger.py
@@ -0,0 +1,11 @@
+from app.utils.logger import logger
+
+logger.info("FlowFi Started")
+
+logger.info("Testing Logger")
+
+logger.warning("This is Warning")
+
+logger.error("This is Error")
+
+logger.success("Logger Working Successfully")
