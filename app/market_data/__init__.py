21b9367e45215f4e64edf008ca6d251596ed30b5
lokeshlodhi1 <choice.employee1130@gmail.com>
2026-06-29 15:30:45 +0530

    Update __init__.py

--- app/market_data/__init__.py
+++ app/market_data/__init__.py
@@ -9,3 +9,7 @@ from .historical_loader import HistoricalLoader
 from .live_loader import LiveLoader
 from .symbol_manager import SymbolManager
 self.database = DatabaseService()
+self.telegram = TelegramService(
+    config.TELEGRAM_TOKEN,
+    config.TELEGRAM_CHAT_ID
+)
