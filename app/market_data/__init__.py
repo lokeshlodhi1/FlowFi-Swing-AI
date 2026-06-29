4a91a9cd42c9aba817ce891fb99c5b7f78686539
lokeshlodhi1 <choice.employee1130@gmail.com>
2026-06-29 19:01:41 +0530

    Update __init__.py

--- app/market_data/__init__.py
+++ app/market_data/__init__.py
@@ -9,6 +9,7 @@ from .historical_loader import HistoricalLoader
 from .live_loader import LiveLoader
 from .symbol_manager import SymbolManager
 
+self.market_trend = MarketTrend()
 __all__ = [
     "MarketDataService",
     "YahooFinanceProvider",
