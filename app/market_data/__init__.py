b817ce241583ea72cd768a6d370367662f15427a
lokeshlodhi1 <choice.employee1130@gmail.com>
2026-06-29 19:32:47 +0530

    Update __init__.py

--- app/market_data/__init__.py
+++ app/market_data/__init__.py
@@ -8,8 +8,7 @@ from .yahoo_provider import YahooFinanceProvider
 from .historical_loader import HistoricalLoader
 from .live_loader import LiveLoader
 from .symbol_manager import SymbolManager
-self.history = TelegramRepository()
-self.market_trend = MarketTrend()
+
 __all__ = [
     "MarketDataService",
     "YahooFinanceProvider",
