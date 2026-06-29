23cace9c4ae5632e32b434a38be3887e49e10ba1
lokeshlodhi1 <choice.employee1130@gmail.com>
2026-06-29 14:26:02 +0530

    Create market_scheduler.py

---
+++ app/telegram/market_scheduler.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+
+class MarketScheduler:
+
+    SCAN_TIMES = [
+
+        "09:20",
+
+        "10:00",
+
+        "11:00",
+
+        "12:00",
+
+        "13:00",
+
+        "14:00",
+
+        "15:00"
+
+    ]
+
+    def should_scan(self):
+
+        now = datetime.now().strftime("%H:%M")
+
+        return now in self.SCAN_TIMES
